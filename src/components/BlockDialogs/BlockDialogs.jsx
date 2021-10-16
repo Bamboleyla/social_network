87bd27479992bbb46be67cccf70c10144530bdbc
PC <dvorobjevredstar@mail.ru>
2021-10-16 17:02:52 +0500

    lesson №18 - add BlockDialogs

---
+++ src/components/BlockDialogs/BlockDialogs.jsx
@@ -0,0 +1,7 @@
+/* Здесь будет находится все внутренности блока Dialogs */
+
+const BlockDialogs = (props) => {
+  return <div>Dialogs</div>;
+};
+
+export default BlockDialogs;
--- src/pages/startPage/App.js
+++ src/pages/startPage/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import BlockContent from '../../components/BlockContent/BlockContent';
 import BlockHeader from '../../components/BlockHeader/BlockHeader';
 import BlockNavigation from '../../components/BlockNavigation/BlockNavigation';
+import BlockDialogs from '../../components/BlockDialogs/BlockDialogs';
 /* Здесь выводится вся StartPage, которая состоит на данный момент из трех блоков */
 const App = () => {
   return (
     <div className="app-wrapper">
       <BlockHeader />
       <BlockNavigation />
-      <BlockContent />
+      <div className='app-wrapper-content'>
+        <BlockDialogs />
+        {/* <BlockContent /> */}
+      </div>
     </div>
   );
 }
