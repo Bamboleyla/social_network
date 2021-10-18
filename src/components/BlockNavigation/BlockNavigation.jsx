19e75c3bb348c54245ec71d856de4be37a913385
PC <dvorobjevredstar@mail.ru>
2021-10-18 09:28:12 +0500

    lesson №20-NavLink

--- src/components/BlockNavigation/BlockNavigation.jsx
+++ src/components/BlockNavigation/BlockNavigation.jsx
@@ -1,23 +1,34 @@
 /* Здесь будет находится все внутренности блока Navigation */
+import { NavLink } from "react-router-dom";
 import style from "./BlockNavigation.module.css";
 
 const BlockNavigation = () => {
   return (
     <nav className={style.nav}>
       <div>
-        <a href="/contents">Profile</a>
+        <NavLink to="/contents" activeClassName={style.active}>
+          Profile
+        </NavLink>
       </div>
       <div>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs" activeClassName={style.active}>
+          Messages
+        </NavLink>
       </div>
       <div>
-        <a>News</a>
+        <NavLink to="/news" activeClassName={style.active}>
+          News
+        </NavLink>
       </div>
       <div>
-        <a>Music</a>
+        <NavLink to="/music" activeClassName={style.active}>
+          Music
+        </NavLink>
       </div>
       <div>
-        <a>Settings</a>
+        <NavLink to="/settings" activeClassName={style.active}>
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
