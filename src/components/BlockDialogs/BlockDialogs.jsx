68f5ceb50bb19c6cbe798cb7cee4a7eed6d7164b
PC <dvorobjevredstar@mail.ru>
2021-10-20 09:31:39 +0500

    lesson №22-add NavLink in BlockDialogs

--- src/components/BlockDialogs/BlockDialogs.jsx
+++ src/components/BlockDialogs/BlockDialogs.jsx
@@ -1,16 +1,49 @@
+import { NavLink } from "react-router-dom";
 import style from "./BlockDialogs.module.css";
 const BlockDialogs = (props) => {
   return (
     <div className={style.blockDialogs}>
       <div className={style.dialogs}>
-        <div className={style.item + " " + style.active}>Dmitriy</div>
-        <div className={style.item}>Luba</div>
-        <div className={style.item}>Gleb</div>
-        <div className={style.item}>Masha</div>
-        <div className={style.item}>Anya</div>
-        <div className={style.item}>Bulya</div>
-        <div className={style.item}>Victor</div>
-        <div className={style.item}>Lusya</div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/1" activeClassName={style.active}>
+            Dmitriy
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/2" activeClassName={style.active}>
+            Luba
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/3" activeClassName={style.active}>
+            Gleb
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/4" activeClassName={style.active}>
+            Masha
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/5" activeClassName={style.active}>
+            Anya
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/6" activeClassName={style.active}>
+            Bulya
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/7" activeClassName={style.active}>
+            Victor
+          </NavLink>
+        </div>
+        <div className={style.item}>
+          <NavLink to="/dialogs/8" activeClassName={style.active}>
+            Lusya
+          </NavLink>
+        </div>
       </div>
       <div className={style.messages}>
         <div className={style.message}>Hi, have a nice day!</div>
--- src/components/BlockNavigation/BlockNavigation.jsx
+++ src/components/BlockNavigation/BlockNavigation.jsx
@@ -4,27 +4,27 @@ import style from "./BlockNavigation.module.css";
 const BlockNavigation = () => {
   return (
     <nav className={style.nav}>
-      <div>
+      <div className={style.item}>
         <NavLink to="/contents" activeClassName={style.active}>
           Profile
         </NavLink>
       </div>
-      <div>
+      <div className={style.item}>
         <NavLink to="/dialogs" activeClassName={style.active}>
           Messages
         </NavLink>
       </div>
-      <div>
+      <div className={style.item}>
         <NavLink to="/news" activeClassName={style.active}>
           News
         </NavLink>
       </div>
-      <div>
+      <div className={style.item}>
         <NavLink to="/music" activeClassName={style.active}>
           Music
         </NavLink>
       </div>
-      <div>
+      <div className={style.item}>
         <NavLink to="/settings" activeClassName={style.active}>
           Settings
         </NavLink>
