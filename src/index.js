c360b027010ba7a7fc808c9f717237b53848939d
PC <dvorobjevredstar@mail.ru>
2021-10-28 11:02:31 +0500

    lesson №30 transferring <Browser Router> to index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './pages/startPage/App';
 import reportWebVitals from './reportWebVitals';
 import state from "./redux/state.js"
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App state={state} />
+    <BrowserRouter>
+      <App state={state} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/startPage/App.js
+++ src/pages/startPage/App.js
@@ -5,20 +5,17 @@ import BlockHeader from '../../components/BlockHeader/BlockHeader';
 import BlockNavigation from '../../components/BlockNavigation/BlockNavigation';
 import BlockDialogs from '../../components/BlockDialogs/BlockDialogs';
 import { Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 /* Здесь выводится вся StartPage, которая состоит на данный момент из трех блоков */
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <BlockHeader />
-        <BlockNavigation />
-        <div className='app-wrapper-content'>
-          <Route path='/dialogs' render={() => <BlockDialogs state={props.state.dialogsPage} />} />
-          <Route path='/contents' render={() => <BlockContent state={props.state.contentPage} />} />
-        </div>
+    <div className="app-wrapper">
+      <BlockHeader />
+      <BlockNavigation />
+      <div className='app-wrapper-content'>
+        <Route path='/dialogs' render={() => <BlockDialogs state={props.state.dialogsPage} />} />
+        <Route path='/contents' render={() => <BlockContent state={props.state.contentPage} />} />
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
