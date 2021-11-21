b6297038121466e24a47f420fef1906d4e502d73
PC <dvorobjevredstar@mail.ru>
2021-11-21 17:35:04 +0500

    lesson№53 extends react.component

--- src/components/BlockUsers/BlockUsers.jsx
+++ src/components/BlockUsers/BlockUsers.jsx
@@ -2,67 +2,60 @@ import * as axios from "axios";
 import React from "react";
 import style from "./BlockUsers.module.css";
 import user_logo from "./user-default.jpg";
-
-const BlockUsers = (props) => {
-  if (props.users.length === 0) {
+/* Создаем классовую компоненту, мы используем ее в место функциональной, что бы не нарушать принцип чистоты функции, если бы использовали функциональную */
+class BlockUsers extends React.Component {
+  constructor(props) {
+    super(props);
+    /* Так как у нас пустой стейт, делаем запрос на сервер */
     axios.get("http://localhost:5000/users").then((response) => {
-      debugger;
-      props.setUsers(response.data.users);
+      /*  И диспачем его в state через метод setUsers, обратите внимание на this так как любая классовая компонента это объект и обращение к props совершенно другой */
+      this.props.setUsers(response.data.users);
     });
-
-    /* props.setUsers([
-      {
-        id: 1,
-        followed: false,
-        fullName: "Дима",
-        status: "I want to become a web developer",
-        location: { country: "Russia", sity: "Ufa" },
-        ava: "https://znamenitka.ru/uploads/posts/2019-11/dmitriy-shepelev-pokazal-podpischikam-poceluy-s-ekaterinoy-tulupovoy-1.jpg",
-      },
-    ]); */
   }
-  return (
-    <div>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                className={style.photo}
-                src={u.ava != null ? u.ava : user_logo}></img>
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}>
-                  Followed
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}>
-                  Unfollowed
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
             <span>
-              <div>{u.fullName}</div>
-              <div>{u.status}</div>
+              <div>
+                <img
+                  className={style.photo}
+                  src={u.ava != null ? u.ava : user_logo}></img>
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}>
+                    Followed
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}>
+                    Unfollowed
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.sity}</div>
+              <span>
+                <div>{u.fullName}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{u.location.country}</div>
+                <div>{u.location.sity}</div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default BlockUsers;
