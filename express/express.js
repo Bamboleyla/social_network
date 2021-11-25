0c4121f3e8aa19b26e7debc9be17dfd04bd8f9f0
PC <dvorobjevredstar@mail.ru>
2021-11-25 11:07:08 +0500

    update user.json and server api

--- express/express.js
+++ express/express.js
@@ -10,37 +10,47 @@ const fs = require("fs"); //для чтения и записи в этот фа
 const PORT = 5000;
 //const filePath = "task.json";
 const users = require('./users.json');//Читаем файл users.json, чтобы знать сколько у нас здесь пользователей
-
+//Запускаем corse для кросбраузерной, безошибочной работы сервера и преложения на одной локальной машине
 app.use(cors());
 app.use(express.json());
+//функция которая возвращает текущее время;
+let now = () => new Date().toLocaleTimeString();
 
 app.get("/home", function (request, response) { // Определяем обработчик для маршрута "/"
     response.sendFile(__dirname + '/express.html'); //Определяем ответ на запрос
 });
 
-console.log(__dirname);
-
 app.get("/users", function (request, response) {
-    console.log("ПОЛУЧЕН ЗАПРОС НА ПОЛУЧЕНИЕ USERS") // Определяем обработчик для маршрута "/"
-    let total_users = users.users.length;            //total_users сколько всего у нас пользователей
-    let numberPage = request.query.page;             //указываем, пока жестко какую страницу нужно выгрузить на клиент
-    let totalPages = Math.ceil(total_users / 10);    //totalPages сколько всего будет страниц с пользователями
-    let usersList = (usersArr, number) => {          //Функция которая делит общее количество пользователей на 10 и возврашает десяток соответствующий номеру страницы
-        let result = [];
-        let start = number === 1 ? 0 : (number - 1) * 10;
-        let finish = number === 1 ? 10 : number * 10;
-        let end = finish >= total_users ? total_users : finish;
-        for (start; start < end; start++) {
-            result.push(usersArr[start])
+    let userId = request.query.userId;                //Проверяем наличие userId, если есть значит запрос за информацией по одному конкретному пользователю, если undefined, то тогда за страницей с users 
+    if (!userId) {
+        console.log(`${now()} Получен запрос на получение USERS`) // Определяем обработчик для маршрута "/"
+        let total_users = users.users.length;            //total_users сколько всего у нас пользователей
+        let numberPage = request.query.page;             //указываем, пока жестко какую страницу нужно выгрузить на клиент
+        let totalPages = Math.ceil(total_users / 10);    //totalPages сколько всего будет страниц с пользователями
+        let usersList = (usersArr, number) => {          //Функция которая делит общее количество пользователей на 10 и возврашает десяток соответствующий номеру страницы
+            let result = [];
+            let start = number === 1 ? 0 : (number - 1) * 10;
+            let finish = number === 1 ? 10 : number * 10;
+            let end = finish >= total_users ? total_users : finish;
+            for (start; start < end; start++) {
+                result.push(usersArr[start])
+            };
+            return result;
         };
-        return result;
+        //Формируем ответ
+        let result = { "users": usersList(users.users, numberPage), "numberPage": numberPage, "totalPages": totalPages, "totalUsers": total_users }
+
+        response.send(result); //Отправляем ответ
+        console.log(`${now()} Данные USERS отправлены`)
+    }
+    else {
+        console.log(`${now()} Получен запрос на получение информации о user ${userId}`)
+        find_a_user = (id) => users.users.find((el) => el.id == id);
+        let result = { "user": find_a_user(userId) };
+        response.send(result); //Отправляем ответ
+        console.log(`${now()} Пользователь ${userId} найден, данные отправлены`)
     };
-    //Формируем ответ
-    let result = { "users": usersList(users.users, numberPage), "numberPage": numberPage, "totalPages": totalPages, "totalUsers": total_users }
-
-    response.send(result); //Отправляем ответ
 });
-
 
 
 
@@ -51,24 +61,24 @@ app.get("/users", function (request, response) {
 }); */
 /***************************ШАБЛОН ДЛЯ POST ЗАПРОСА*************************************************/
 /* app.post("/task/1", jsonParser, function (req, res) {
-
+ 
     if (!req.body) return res.sendStatus(400);
     let newTask = req.body.task;
     console.log("ПРИШЕЛ ЗАПРОС НА ДОБАВЛЕНИЕ НОВОГО ЗАДАНИЯ")
     console.log(newTask);
-
+ 
     let data = fs.readFileSync(filePath, "utf8");
     let users = JSON.parse(data);
-
+ 
     users.task.push(newTask);
     console.log("ЗАПРОС ОБРАБОТАН")
-
+ 
     data = JSON.stringify(users);
     // перезаписываем файл с новыми данными
     fs.writeFileSync("task.json", data);
-
+ 
     console.log("СЕРВЕР ОЖИДАЕТ НОВОГО ЗАПРОСА")
     res.send(users);
 }); */
 
-app.listen(PORT, () => { console.log(`ЛОКАЛЬНЫЙ СЕРВЕР ЗАПУШЕН ПО АДРЕСУ Localhost: ${PORT}`) }); //Начинаем слушать порт 5000
+app.listen(PORT, () => { console.log(`${now()} ЛОКАЛЬНЫЙ СЕРВЕР ЗАПУШЕН ПО АДРЕСУ Localhost: ${PORT}`) }); //Начинаем слушать порт 5000
